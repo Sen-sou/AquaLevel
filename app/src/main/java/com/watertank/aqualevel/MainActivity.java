6ed431578dfe84d43e129f33ca3eb5aa918856a9
Sen-sou <128981362+Sen-sou@users.noreply.github.com>
2024-03-08 23:52:36 +0530

    Layout Progress

--- app/src/main/java/com/watertank/aqualevel/MainActivity.java
+++ app/src/main/java/com/watertank/aqualevel/MainActivity.java
@@ -2,13 +2,25 @@ package com.watertank.aqualevel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView toolbarText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbarText = findViewById(R.id.toolbarText);
+        SpannableString spannableString = new SpannableString("AQUALEVEL");
+        spannableString.setSpan(new ForegroundColorSpan(0xFF018786),
+                0, 4, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        toolbarText.setText(spannableString);
     }
 }
